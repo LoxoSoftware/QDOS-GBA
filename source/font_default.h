2a48d51e24a70df8dc2b5e7a74b6875efab8fe50
Loxo <ishootupewpew@gmail.com>
2025-04-24 16:35:40 +0200

    Font polish

--- source/font_default.h
+++ source/font_default.h
@@ -7,7 +7,7 @@
 //	+ bitmap not compressed
 //	Total size: 2048 = 2048
 //
-//	Time-stamp: 2025-04-15, 21:23:30
+//	Time-stamp: 2025-04-24, 16:33:03
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
